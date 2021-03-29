1bc26be9c2c62e7551baba472a57983f8e55f4a1
evg_dev <e.kabakov@phoenix-cg.ru>
2021-03-29 22:37:23 +0500

    1-module \ 3-task finished

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length > 0) {
+    const newStr = str.charAt(0).toUpperCase() + str.slice(1, str.length);
+    return newStr;
+  } else if (str.length === 1) {
+    return str.toUpperCase();
+  }
+  else {
+    return '';
+  }
 }
