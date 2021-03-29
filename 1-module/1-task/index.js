3c2dacf6394f55609e2398ad9be42efce47ddc66
evg_dev <e.kabakov@phoenix-cg.ru>
2021-03-29 18:50:32 +0500

    1-module \ 1-task finished

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  if (n > 1) {
+    var result = n;
+    for (n; n > 1; ) {
+      result = Number(result * (n - 1));
+      n--;
+    }
+    return result;   
+  } else {
+    return 1;
+  }
 }
