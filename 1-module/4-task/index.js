ac5004f9b71749bb1e0f9b89c5e76641d5ad7eed
evg_dev <e.kabakov@phoenix-cg.ru>
2021-04-01 12:21:15 +0500

    1-module \ 4-task finished

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,22 @@
 function checkSpam(str) {
-  // ваш код...
+  str = str.toLowerCase().replace(/\s/g, '');
+  const spamWords = ['1xBet','xxx'];
+  let totalResult = 0;
+  spamWords.some(function(el,i) {
+    el = el.toLowerCase();
+    let strSpam = str.indexOf(el);
+    if (strSpam >= 0) {
+      totalResult += 1;
+    } else {
+      if (totalResult > 0) {
+        totalResult = totalResult - 1;
+      }
+    }
+    return totalResult;
+  });
+  if (totalResult > 0) {
+    return true;
+  } else {
+    return false;
+  }  
 }
