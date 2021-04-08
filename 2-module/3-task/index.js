45bf46c53fad2e3d12d6ee298582c9c866d60160
evg_dev <e.kabakov@phoenix-cg.ru>
2021-04-08 14:58:28 +0500

    2-module \ 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
+  
 };
+calculator.read = function(x, y) {
+  calculator.a = x;
+  calculator.b = y;
+}
+calculator.sum = function(x, y) {
+  return calculator.a + calculator.b;
+}
+calculator.mul = function(x, y) {
+  return calculator.a * calculator.b;
+}
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
