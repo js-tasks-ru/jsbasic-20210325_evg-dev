cb31aa3608b185881c2084a9aff2000b175590ac
evg_dev <e.kabakov@phoenix-cg.ru>
2021-04-12 22:57:01 +0500

    5-module \ 2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const container = document.querySelector('#text');
+  btn.addEventListener('click', () => {
+    if (container.getAttribute('hidden') !== null) {
+      container.removeAttribute('hidden');
+    } else {
+      container.setAttribute('hidden', true);
+    }
+  });
 }
